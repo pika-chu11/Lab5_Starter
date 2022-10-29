baede2631d8caafacdd0c38b13cab596839023b8
Pikachu <xuh013@ucsd.edu>
2022-10-29 16:10:14 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,19 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  // Change image
+  const selectElement = document.querySelector('.ice-cream');
+  selectElement.addEventListener('change', (event => {
+      const result = document.querySelector('.result');
+      changeImage(result);
+  }) )
+}
+
+function changeImage(options) 
+{
+  if (options = "air-horn"){
+    let image = document.querySelector("#expose img");
+    image.src = 'assets/images/car-horn.svg';
+  }
 }
